ea2a6c6f366a2d2e3ac1a38e2e9213d53fef88c6
Nanda Oberusti <56298239+oberustinanda11@users.noreply.github.com>
2024-06-07 13:20:49 +0700

    Delete inventory directory

--- inventory/halaman.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "fungsi.php"; ?>
-
-<?= template_header("Sistem Manajemen Inventory") ?>
-<head>
-	<title>Sistem Manajemen Inventory Sejutakado</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-
